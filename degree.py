b3d87090d2ff049e49d099cd4fc2e7c4913da06b
ankita <01fe24bca204@kletech.ac.in>
2026-01-18 13:27:14 +0530

    My second

--- degree.py
+++ degree.py
@@ -1,3 +1,8 @@
-celsius=float(input("Enter the tempurature in celsuis:"))
-farhenheit=(celsius*9/5)+32
-print("the tempurature in farhenheit is:", farhenheit)
+def celsius_to_fahrenheit(celsius):
+    return (celsius * 9/5) + 32
+
+
+if __name__ == "__main__":
+    celsius = float(input("Enter temperature in Celsius: "))
+    fahrenheit = celsius_to_fahrenheit(celsius)
+    print("Temperature in Fahrenheit:", fahrenheit)
